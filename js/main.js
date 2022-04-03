a0acbbd6ffffc3aed72e0a00ea414d8a4763a805
highjeans <highjeans11@hotmail.com>
2022-04-03 08:17:22 -0700

    Add sliding sidebar from left

--- js/main.js
+++ js/main.js
@@ -4,10 +4,39 @@ const testScores = {
     "joe": 90
 };
 
+const classes = ["⭐️", "🦸🏽‍️", "🖌️", "👌🏽"]
+
 function getStudentScore(student) {
     return testScores[student];
 }
 
 function setStudentScore(student, score) {
     testScores[student] = score;
+}
+
+function openNav() {
+    document.getElementById("menu").style.width = "144px"
+    var items = document.getElementsByClassName("item");
+    for (var elementKey in items) {
+        items[elementKey].textContent += " - Class " + (parseInt(elementKey) + 1);
+    }
+}
+
+function closeNav() {
+    var items = document.getElementsByClassName("item");
+    for (var elementKey in items) {
+        items[elementKey].textContent = classes[elementKey];
+    }
+    document.getElementById("menu").style.width = "20px"
+}
+
+function toggleMenu(menu) {
+    menu.classList.toggle('change')
+    const width = document.getElementById("menu").style.width;
+    if (width === "20px" || width === "") {
+        openNav()
+    }
+    else {
+        closeNav()
+    }
 }
